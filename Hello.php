2babf4400cf43df1610b3246294f0bfbf5920790
Anytakes <aleks.vladimir.loptev@gmail.com>
2023-08-27 15:13:53 +0300

    Over

--- Hello.php
+++ Hello.php
@@ -1,6 +1,10 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 <><><>
 Zamens for master experiment
+=======
+For stash number 2
+>>>>>>> Stashed changes
 =======
 For stash number 2
 >>>>>>> Stashed changes
